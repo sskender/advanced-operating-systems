796eb771fedec1a57ea94d9eb0485f846a3e05b1
Sven Skender <sskender@pm.me>
2022-11-11 19:11:29 +0100

    [WIP] In case of fire 2

--- 02_pipes/lamport-proc.py
+++ 02_pipes/lamport-proc.py
@@ -1,14 +1,32 @@
 import logging
 import time
 import random
-from multiprocessing import Process, Pipe
+from multiprocessing import Process, Pipe, Array, Manager
+from multiprocessing.managers import BaseManager
+import ctypes
 
 
 NUM_PIPES = 6
 NUM_WORKERS = 3
 
 
-DATABASE = []
+class Database:
+    def __init__(self):
+        self.storage = []
+    def add(self, item):
+        self.storage.append(item)
+    def update(self, pi, clock, ko_count):
+        idx = pi - 1
+        self.storage[idx].clock = clock
+        self.storage[idx].ko_count = ko_count
+        print(self.storage[idx])
+    def __repr__(self):
+        return self.__str__()
+    def __str__(self):
+        return f"{str(self.storage)}"
+
+class CustomManager(BaseManager):
+    pass
 
 
 class DbEntry:
@@ -46,6 +64,12 @@ class Message:
             return "ACK"
         if self.msg_type == Message.END:
             return "END"
+    def __eq__(self, other):
+        if (isinstance(other, Message)):
+            return self.msg_type == other.msg_type \
+                and self.sender_pi == other.sender_pi \
+                    and self.sender_clock == other.sender_clock
+        return False
     def __lt__(self, other):
         if self.sender_clock < other.sender_clock:
             return True
@@ -70,13 +94,21 @@ class RequestQueue:
             self.queue.sort()
     def size(self):
         return len(self.queue)
-    def remove(self, req):
-        # TODO implement
-        pass
-    def is_my_request_first(self):
+    def remove(self, req: Message):
+        req.msg_type = Message.REQ  # override for quueue
+        self.queue.remove(req)
+    def get_first(self):
         if self.size() > 0:
-            return self.queue[0].sender_pi == self.pi
-        return False
+            return self.queue[0]
+        return None
+    def is_my_request_first(self):
+        f = self.get_first()
+        if f is None:
+            return False
+        return f.sender_pi == self.pi
+        #if self.size() > 0:
+        #    return self.queue[0].sender_pi == self.pi
+        #return False
     def __repr__(self):
         return self.__str__()
     def __str__(self):
@@ -123,16 +155,18 @@ def read_message(pi, pipe) -> Message:
 
 
 
-def run_database_job(pi, clock, ko_count):
+def run_database_job(db: Database, pi, clock, ko_count):
     # TODO locking shared database ???
     logger = logging.getLogger()
     logger.setLevel(logging.DEBUG)
 
     logging.info(f"Pi {pi} is starting a database job")
     idx = pi - 1
-    DATABASE[idx].clock = clock
-    DATABASE[idx].ko_count = ko_count
-    logging.info(f"pi {pi} updated database: {DATABASE}")
+    print(db)
+    db.update(pi, clock, ko_count)
+    #DATABASE[idx].clock = clock
+    #DATABASE[idx].ko_count = ko_count
+    logging.info(f"pi {pi} updated database: {db}")
     r = random.randint(100, 2000) / 1000
     time.sleep(r)
     logging.info(f"Pi {pi} finished database job")
@@ -144,17 +178,20 @@ def get_messages(pi, pipes):
             yield read_message(pi, pipe)
 
 
-def run_worker(pi, pipes_read, pipes_write):
+def run_worker(db, pi, pipes_read, pipes_write):
     # TODO param send array and recv array and loop while 1
     logger = logging.getLogger()
     logger.setLevel(logging.DEBUG)
     logging.info(f"Starting process {pi}")
+    #logging.info(f"pi {pi} updated database: {db[:]}")
+    #print(db)
     time.sleep(1)
+    #return
 
     requests_queue = RequestQueue(pi)
     clock = 0
     ko_count = 0
-    ko_count_break_point = 5
+    ko_count_break_point = 2
     ack_required = len(pipes_write)
     ack_received = 0
 
@@ -198,6 +235,15 @@ def run_worker(pi, pipes_read, pipes_write):
                     clock = max(clock,  msg.sender_clock) + 1
                     logger.debug(f"pi {pi} clock updated: {clock}")
 
+                # received end messga for someone's exit from K.O.
+                if msg.is_end():
+                    clock = max(clock,  msg.sender_clock) + 1
+                    logger.debug(f"pi {pi} clock updated: {clock}")
+                    logger.warn(f"pi {pi} queue before remove {requests_queue} to remove {msg}")
+                    requests_queue.remove(msg)
+                    logger.warn(f"pi {pi} queue after remove {requests_queue}")
+                    # TODO implement
+
             # check if I can go to K.O. now
             if ack_received == ack_required \
                 and requests_queue.is_my_request_first():
@@ -208,9 +254,30 @@ def run_worker(pi, pipes_read, pipes_write):
 
         if ko_count < ko_count_break_point:
             logger.debug(f"pi {pi} just received all acks: clock = {clock} queue = {requests_queue} and entering KO")
+            
+            my_req = requests_queue.get_first()
+            assert(my_req is not None)
+            assert(my_req.sender_pi == pi)
+
             ko_count += 1
             ack_received = 0
-            run_database_job(pi, clock, ko_count)
+
+            # do job
+            run_database_job(db, pi, clock, ko_count)
+
+
+
+            # TODO remove request from local
+            logger.warn(f"pi {pi} queue before remove {requests_queue}")
+            requests_queue.remove(my_req)
+            logger.warn(f"pi {pi} queue after remove {requests_queue}")
+
+            # TODO metadata from REQ must be sent in END
+
+            # notify everyone upon exit
+            msg = Message(Message.END, my_req.sender_pi, my_req.sender_clock)
+            logger.debug(f"pi {pi} is sending END msg {msg}")
+            broadcast_message(pi, pipes_write, msg)
 
         #
 
@@ -316,6 +383,7 @@ def get_pipes_write(pipes, pi):
     if pi == 3:
         return [pipes[3][1], pipes[4][1]]
 
+# TODO remove unused
 def init_database():
     for i in range(NUM_WORKERS):
         pi = i + 1
@@ -324,35 +392,64 @@ def init_database():
     logging.info(DATABASE)
 
 def main():
-    pipes = []
+    CustomManager.register('Database', Database)
+    #manager = CustomManager()
+    #db = manager.Database()
+    #db = Array(Message, NUM_WORKERS)
     logging.debug('Starting main')
 
-    # init database
-    init_database()
+    with CustomManager() as manager:
+        db = manager.Database()
+        print(db)
 
-    # create pipes
-    # TODO formula
-    for _ in range(NUM_PIPES):
-        r, w = Pipe()
-        pipes.append((r, w))
-    logging.debug(f"pipes: {pipes}")
+        for i in range(NUM_WORKERS):
+            pi = i + 1
+            entry = DbEntry(pi, 0, 0)
+            db.add(entry)
+            #db[i] = entry
+        logging.info(db)
+        
+        #return
 
-    #  sadrži identifikator procesa, vrijednost logičkog sata procesa te broj ulazaka u kritički odsječak procesa. 
+
+        #return
+        #print(db)
+        pipes = []
+        
+
+        # init database
+        #init_database()
+        #for i in range(NUM_WORKERS):
+        #    pi = i + 1
+        #    entry = DbEntry(pi, 0, 0)
+        #    db[i] = entry
+        #logging.info(db[:])
+
+        #return
+
+        # create pipes
+        # TODO formula
+        for _ in range(NUM_PIPES):
+            r, w = Pipe()
+            pipes.append((r, w))
+        logging.debug(f"pipes: {pipes}")
+
+        #  sadrži identifikator procesa, vrijednost logičkog sata procesa te broj ulazaka u kritički odsječak procesa. 
 
 
-    # create processes
-    procs = []
-    for i in range(NUM_WORKERS):
-        pi = i + 1
-        pipes_read = get_pipes_read(pipes, pi)
-        pipes_write = get_pipes_write(pipes, pi)
-        proc = Process(target=run_worker, args=(pi, pipes_read, pipes_write, ))
-        logging.debug(f"proc: {proc} pi: {pi} pipes_read: {pipes_read} pipes_write: {pipes_write}")
-        procs.append(proc)
-        proc.start()
+        # create processes
+        procs = []
+        for i in range(NUM_WORKERS):
+            pi = i + 1
+            pipes_read = get_pipes_read(pipes, pi)
+            pipes_write = get_pipes_write(pipes, pi)
+            proc = Process(target=run_worker, args=(db, pi, pipes_read, pipes_write, ))
+            logging.debug(f"proc: {proc} pi: {pi} pipes_read: {pipes_read} pipes_write: {pipes_write}")
+            procs.append(proc)
+            proc.start()
 
-    for i in range(NUM_WORKERS):
-        procs[i].join()
+        for i in range(NUM_WORKERS):
+            procs[i].join()
 
 
 if __name__ == "__main__":
